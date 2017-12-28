e3966c5a079f5c55f274a2927e1633c366c15ccc
Kent Carmine <kentcarmine@gmail.com>
2017-12-28 17:22:40 -0600

    Set up MongoDB

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,1 +1,42 @@
 'use strict';
+
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+let obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log("Error connecting to MongoDB server.", err);
+    return;
+  }
+  console.log("Connected to MongoDB server.");
+
+  // db.collection('Todos').insertOne({
+  //   text: "Something to do",
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     console.log("Error inserting Todo", err);
+  //     return;
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: "Bob",
+  //   age: 40,
+  //   location: "New York"
+  // }, (err, result) => {
+  //   if (err) {
+  //     console.log("Error inserting User", err);
+  //     return;
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
+
+  db.close();
+});
