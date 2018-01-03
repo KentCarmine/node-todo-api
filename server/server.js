ee0cb6acfc2386164ddb9714937e479766d7977f
Kent Carmine <kentcarmine@gmail.com>
2018-01-03 15:32:50 -0600

    Add GET todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const {ObjectID} = require('mongodb');
 let express = require('express');
 let bodyParser = require('body-parser');
 
@@ -29,6 +30,25 @@ app.get('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+    return;
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(todo) {
+      res.send({todo});
+    } else {
+      res.status(404).send();
+    }
+  }).catch((err) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(3000, () => {
